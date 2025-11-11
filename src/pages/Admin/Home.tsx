fbb2e5c2ac7cde5fa9f6cd766e3f68021db797d4
galactic-nebula <59515708+galactic-nebula@users.noreply.github.com>
2025-11-11 17:56:16 -0500

    Added clickable functionality, redirects

--- src/pages/Admin/Home.tsx
+++ src/pages/Admin/Home.tsx
@@ -9,68 +9,54 @@ export default function AdminHome(){
   //replace info  with call to backend
   //name is name of worker
     const infol = [{
-  title:"",
-  xaxis: "X axis label",
-  yaxis: 'y axis label',
-  xdata: [1, 2, 3, 4],
-  ydata: [1, 2, 3, 4],
+  title:"Appointment Trends",
+  xaxis: "Day",
+  yaxis: 'Number of Appointments',
+  xdata: ["Mon", "Tues", "Wed", "Thurs", "Fri"],
+  ydata: [1, 2, 3, 4, 3, 5],
+  period:'week',
+  page:"./ApptTrends",
+}, {
+  title:"Platform Uptime",
+  xaxis: "Day",
+  yaxis: 'Uptime (percent)',
+  xdata: ["Mon", "Tues", "Wed", "Thurs", "Fri"],
+  ydata: [100, , 99.7, 99.8, 99.9, 99.6],
+  period:'week',
+  page:"./Uptime",
+}, {
+  title:"Salon Revenue",
+  xaxis: "Day",
+  yaxis: 'Average Revenue (thousands)',
+  xdata: ["Mon", "Tues", "Wed", "Thurs", "Fri"],
+  ydata: [1, 2, 3, 4, 3, 5],
+  period:'week',
+  page:"./Revenue",
+}, {
+  title:"User Engagement",
+  xaxis: "Day",
+  yaxis: 'Average Time Spent on Page (minutes)',
+  xdata: ["Mon", "Tues", "Wed", "Thurs", "Fri"],
+  ydata: [20, 10, 30, 40, 37, 52],
   period:'month',
-  page:"",
-}, {
-  id: 2,
-  name: 'Idalina Vater',
-  salon: 'Hair&Care',
-  time:'4:00 PM',
-  date: '9/22/2025',
-}, {
-  id: 3,
-  name: 'Idalina Vater',
-  salon: 'Hair&Care',
-  time:'1:00 PM',
-  date: '8/22/2025',
-}, {
-  id: 4,
-  name: 'Idalina Vater',
-  salon: 'Hair&Care',
-  time:'5:00 PM',
-  date: '7/22/2025',
-}, {
-  id: 5,
-  name: 'Idalina Vater',
-  salon: 'Hair&Care',
-  time:'3:00 PM',
-  date: '5/22/2025',
+  page:"./Engagement",
 }];
 const infob = [{
-  xaxis: "X axis label",
-  yaxis: 'y axis label',
-  xdata: [1, 2, 3, 4],
-  ydata: [1, 2, 3, 4],
-  period:'month',
+  title:"Customer Retention",
+  xaxis: "Day",
+  yaxis: 'Returning Customers',
+  xdata: ["Mon", "Tues", "Wed", "Thurs", "Fri"],
+  ydata: [1, 2, 3, 4, 3, 5],
+  period:'week',
+  page:"./CustomerRetention",
 }, {
-  id: 2,
-  name: 'Idalina Vater',
-  salon: 'Hair&Care',
-  time:'4:00 PM',
-  date: '9/22/2025',
-}, {
-  id: 3,
-  name: 'Idalina Vater',
-  salon: 'Hair&Care',
-  time:'1:00 PM',
-  date: '8/22/2025',
-}, {
-  id: 4,
-  name: 'Idalina Vater',
-  salon: 'Hair&Care',
-  time:'5:00 PM',
-  date: '7/22/2025',
-}, {
-  id: 5,
-  name: 'Idalina Vater',
-  salon: 'Hair&Care',
-  time:'3:00 PM',
-  date: '5/22/2025',
+  title:"Loyalty Program",
+  xaxis: "Day",
+  yaxis: 'Loyalty Program Usage',
+  xdata: ["Mon", "Tues", "Wed", "Thurs", "Fri"],
+  ydata: [1, 2, 3, 4, 3, 5],
+  period:'week',
+  page:"./Loyalty",
 }];
 //backend call for x axis, y axis, dataset, time period AND put chart type in there
 // can map BUT need to separate line vs bar charts
@@ -78,12 +64,12 @@ const infob = [{
 //props --> chart={"line"} x={item.xaxis} y={item.yaxis} data={item.dataset} time={item.period}
 //props --> chart={"bar"} ^^^ and same as above
 //metrics
-// - platform uptime - line graph, continuous average (percentage)
-// - customer retention - customer retention (monthly bar graph)
-// - salon revenue - average salon revenue over x months
-// - loyalty program - number of salons with loyalty program bar graph with months
-// - line graph of daily/weekly/monthly appointments booked
-// - user engagement - average time spent on page (daily/weekly/monthly) line graph
+// - platform uptime - line graph, continuous average (percentage) LINE
+// - customer retention - customer retention (monthly bar graph) BAR
+// - salon revenue - average salon revenue over x months LINE
+// - loyalty program - number of salons with loyalty program bar graph with months BAR
+// - line graph of daily/weekly/monthly appointments booked LINE
+// - user engagement - average time spent on page (daily/weekly/monthly) line graph LINE
 //create button for daily weekly monthly on admin card --> redoes backend call? --> maybe think about this later --> LATER???? for now do one solid thing and put it in there commented out to discuss with backend
 //
     const linecharts = infol.map(item => <Admin chart={"line"} xlabel={item.xaxis} ylabel={item.yaxis} xdata={item.xdata} ydata={item.ydata} time={item.period} />);
