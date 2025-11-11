3432c201a6295c1223bad024b24ec3a860cdd809
galactic-nebula <59515708+galactic-nebula@users.noreply.github.com>
2025-11-11 17:19:20 -0500

    Dashboard cards finished

--- src/pages/Admin/Home.tsx
+++ src/pages/Admin/Home.tsx
@@ -8,12 +8,45 @@ import Admin from "../../components/DashboardCard";
 export default function AdminHome(){
   //replace info  with call to backend
   //name is name of worker
-    const info = [{
-  id: 1,
+    const infol = [{
+  title:"",
+  xaxis: "X axis label",
+  yaxis: 'y axis label',
+  xdata: [1, 2, 3, 4],
+  ydata: [1, 2, 3, 4],
+  period:'month',
+  page:"",
+}, {
+  id: 2,
   name: 'Idalina Vater',
   salon: 'Hair&Care',
-  time:'2:00 PM',
-  date: '10/22/2025',
+  time:'4:00 PM',
+  date: '9/22/2025',
+}, {
+  id: 3,
+  name: 'Idalina Vater',
+  salon: 'Hair&Care',
+  time:'1:00 PM',
+  date: '8/22/2025',
+}, {
+  id: 4,
+  name: 'Idalina Vater',
+  salon: 'Hair&Care',
+  time:'5:00 PM',
+  date: '7/22/2025',
+}, {
+  id: 5,
+  name: 'Idalina Vater',
+  salon: 'Hair&Care',
+  time:'3:00 PM',
+  date: '5/22/2025',
+}];
+const infob = [{
+  xaxis: "X axis label",
+  yaxis: 'y axis label',
+  xdata: [1, 2, 3, 4],
+  ydata: [1, 2, 3, 4],
+  period:'month',
 }, {
   id: 2,
   name: 'Idalina Vater',
@@ -51,9 +84,10 @@ export default function AdminHome(){
 // - loyalty program - number of salons with loyalty program bar graph with months
 // - line graph of daily/weekly/monthly appointments booked
 // - user engagement - average time spent on page (daily/weekly/monthly) line graph
-//create button for daily weekly monthly on admin card --> redoes backend call? --> maybe think about this later
+//create button for daily weekly monthly on admin card --> redoes backend call? --> maybe think about this later --> LATER???? for now do one solid thing and put it in there commented out to discuss with backend
 //
-    const listItems = info.map(item => <Admin key={item.id} name={item.name} salon={item.salon} time={item.time} date={item.date} />);
+    const linecharts = infol.map(item => <Admin chart={"line"} xlabel={item.xaxis} ylabel={item.yaxis} xdata={item.xdata} ydata={item.ydata} time={item.period} />);
+    const barcharts = infob.map(item => <Admin chart={"bar"} xlabel={item.xaxis} ylabel={item.yaxis} xdata={item.xdata} ydata={item.ydata} time={item.period} />);
     return (
         <div>
             <h1>
@@ -61,7 +95,8 @@ export default function AdminHome(){
             </h1>
             <br/>
             <br/>
-            <div>{listItems}</div>
+            <div>{linecharts}</div>
+            <div>{barcharts}</div>
             <br/>
         </div>
 
